4ae4920dc59df8af0be64cc55ae4bf1ba37b8855
OtnielaSolofo <solofootniela@gmail.com>
2025-09-30 14:56:55 +0200

    change app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(findProperty("MYAPP_KEYSTORE_FILE") as String)
+            storePassword = findProperty("MYAPP_KEYSTORE_PASSWORD") as String
+            keyAlias = findProperty("MYAPP_KEY_ALIAS") as String
+            keyPassword = findProperty("MYAPP_KEY_PASSWORD") as String
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -25,6 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
